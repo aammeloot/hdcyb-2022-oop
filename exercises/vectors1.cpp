cf2adcf4a5233a8d3a56d389b68a8d700d04dc0d
Aurélien Ammeloot <aammeloot@gmail.com>
2022-09-27 15:47:25 +0100

    exercise 1 vectors

---
+++ exercises/vectors1.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Input the names
+void inputName(vector<string> &snames)
+{
+    // Input 5 names
+    for (int counter = 0; counter < 5; counter++)
+    {
+        cout << "Enter the name of a student:\n";
+        string sname;
+        cin >> sname;
+        // Add to the vector
+        snames.push_back(sname);
+    }
+}
+
+void displayNames(const vector<string> &snames)
+{   // Roll on all the names and display
+    for (string name: snames)
+    {
+        cout << name << endl;
+    }
+}
+
+int searchName(const vector<string> &snames, const string &keyword)
+{
+    for(int index = 0; index < snames.size(); index++)
+    {
+        if(snames[index] == keyword)
+        {
+            return index;
+        }
+    }
+    return NULL;
+}
+
+// Main program
+int main(int argc, char const *argv[])
+{
+    vector<string> studentNames;
+    
+    inputName(studentNames);
+    displayNames(studentNames);
+    
+    // Define a search term
+    string searchTerm;
+    cout << "Enter a name: ";
+    cin >> searchTerm;
+    int result = searchName(studentNames, searchTerm);
+    if(!result)
+    {
+        cout << "Name not found.\n";
+    }
+    else
+    {
+        cout << "Name found at index: " << result << endl;
+    }
+
+    return 0;
+}
